bf3e64fff34d419ddf476a309711efc0ad1f4f04
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-10-17 23:40:28 -0400

    done

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(20),
+  age SMALLINT,
+  gender VARCHAR(10),
+  color VARCHAR(10),
+  temperament VARCHAR(20),
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO bears(id,name,age,gender,color,temperament,alive) VALUES (1,'Mr. Chocolate',19,'male','brown','nice',true),
+(2,'Rowdy',10,'male','white','aggressive',false),(3,'Tabitha',12,'female','red','even keeled',true),(4,'Sergeant Brown',25,'female','grey','balanced',true),
+(5, 'Melissa', 17, 'female','purple','depressed',false),(6,'Grinch',5,'male','green','stingy',true),(7, 'Wendy',2, 'female','pink','head strong',false),(8,NULL,10,'male','beige','aggressive',true)
+
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed
+-- (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
